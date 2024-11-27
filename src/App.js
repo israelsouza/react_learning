c6e8224e02d443c1810604b474bfaed33ad9dda3
Israel de Souza <israellimas@hotmail.com>
2024-11-27 13:22:46 -0300

    JSX

--- src/App.js
+++ src/App.js
@@ -1,13 +1,36 @@
 import './App.css';
 
 function App() {
+
+  // every code is here, inside the App function
+  // 'upper' only imports
+  // 'lower' only exports
+
+  const oneName = 'IsRael'
+  const caminhoDaURL = 'https://placecats.com/millie_neo/300/200'
+
+  function sum(a,b) {
+    return a + b;
+  }
+
   return (
+
+    // only one Parent // use className='' instead class=''
     <div className="App">
 
       <p>Hello world!</p>
       <span>in the React app</span>
 
+        <div>
+          <p> Using variables, my name is {oneName}</p>
+          <p> I can also doing some functions here, but that is not common to doing</p>
+          <p> The sum of 4 and 3 is: {sum(4,3)}</p>
+          <img src={caminhoDaURL} alt='two cats being cuts' />
+        </div>
+
     </div>
+    
+
   );
 }
 
