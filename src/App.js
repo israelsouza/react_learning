5940508a8fae119dcb8f8e22d73b6e0a57dd3dd4
Israel de Souza <israellimas@hotmail.com>
2024-11-30 20:21:56 -0300

    métodos por props
    - utilização de componente pai (o btn) no componente filho (evendo) e adicionando métodos nele, que vão ser disparados após interação.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import './App.css';
+import Evento from './components/Evento.js';
 import Form from './components/form/formElements.js'
+
 
 function App() {
 
   return (
 
     <div className="App">
-
         <Form />
-
+        <Evento />
     </div>
 
   );
---
+++ src/components/form/Button.js
@@ -0,0 +1,7 @@
+function Button(props) {
+
+    return <button onClick={props.event}>{props.descricao}</button>
+
+}
+
+export default Button
